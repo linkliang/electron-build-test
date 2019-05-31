71f751d90c61fd7edeaf645bc2f9a7c288393bd5
linkliang <linckyliang@gmail.com>
2019-05-31 11:06:14 -0700

    remove extra git clone command

--- build.py
+++ build.py
@@ -3,10 +3,6 @@ import os
 
 branch = sys.argv[1]
 
-#git_pull_command = 'git pull origin '+branch
-git_clone_command = f"git clone --branch {branch} https://github.com/linkliang/electron-quick-start.git"
-os.system(git_clone_command)
-
 with open('package.template.json', 'r') as file:
     data = file.read().replace('{{branch_name}}', branch)
 with open('package.json', 'w') as file:
